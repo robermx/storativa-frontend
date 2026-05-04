030aa20ffa5728cfac4899266c1cc0e73c05c90c
Roberto Arenas <roberto.arenas2@icloud.com>
2026-05-04 13:16:44 -0600

    fix: vite config env config

--- app/env.d.ts
+++ app/env.d.ts
@@ -2,12 +2,3 @@ declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
 }
-
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string;
-  // añade aquí otras variables que uses...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
---
+++ vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly PORT: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
