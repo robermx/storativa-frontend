884e541809e3bc4e28e1c59d93e7569d7071724e
Roberto Arenas <73556231+robermx@users.noreply.github.com>
2026-07-23 12:22:11 -0600

    feat: update pnpm

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,5 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-  future: {
-    v8_middleware: false,
-    v8_splitRouteModules: false,
-    v8_viteEnvironmentApi: false,
-    v8_passThroughRequests: false,
-    v8_trailingSlashAwareDataRequests: false,
-  },
+  splitRouteModules: false,
 } satisfies Config;
