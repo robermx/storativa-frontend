3d87a791418528601880916d97515cd02c051c44
Roberto Arenas <73556231+robermx@users.noreply.github.com>
2026-07-27 18:30:19 -0600

    feat final section home content

--- app/components/home/FinalSection.tsx
+++ app/components/home/FinalSection.tsx
@@ -1,13 +1,14 @@
 const FinalSection = () => {
   return (
-    <section className="h-dvh flex flex-col items-center justify-center relative px-6 py-32">
+    <section className="h-[calc(100vh-70px)] min-h-100 flex flex-col items-center justify-center relative px-6 py-32">
       <div className="text-center max-w-3xl">
         <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-dark dark:text-light tracking-tighter mb-6">
-          El Ciclo <span className="text-primary">Continúa</span>
+          Tu historia puede crear <span className="text-primary">lazos</span>
         </h2>
         <p className="text-lg md:text-xl text-dark dark:text-light font-light leading-relaxed mb-12">
-          Cada idea que nace lleva dentro el potencial de transformar. Lo que
-          comenzó como un destello ahora es una experiencia que trasciende.
+          Lo que nace de una emoción, un recuerdo o una mirada propia puede
+          convertirse en una historia capaz de cruzar épocas, despertar otras
+          ideas y conectar con quienes encuentran algo de sí mismos en ella.
         </p>
       </div>
     </section>
